5b2cc80882d0c8417e948a4be25600c3577211b3
Erik Barron <todayisagrnday21@gmail.com>
2020-01-15 14:26:22 -0800

    provide division feature

--- js/scripts.js
+++ js/scripts.js
@@ -19,24 +19,38 @@ var divide = function(number1, number2) {
 $(document).ready(function(){
 
   $("form#add").submit(function(event){
-
     event.preventDefault();
     var addNumber1 = parseInt($("#add1").val());
     var addNumber2 = parseInt($("#add2").val());
     var addResult = add(addNumber1, addNumber2);
     $("#addOutput").text(addResult);
-
   });
 
 
   $("form#subtract").submit(function(event){
-
     event.preventDefault();
     var subtractNumber1 = parseInt($("#subtract1").val());
     var subtractNumber2 = parseInt($("#subtract2").val());
     var subtractResult = subtract(subtractNumber1, subtractNumber2);
     $("#subtractOutput").text(subtractResult);
+  })
 
+
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var multiplyNumber1 = parseInt($("#multiply1").val());
+    var multiplyNumber2 = parseInt($("#multiply2").val());
+    var multiplyResult = multiply(multiplyNumber1, multiplyNumber2);
+    $("#multiplyOutput").text(multiplyResult);
+  })
+
+
+  $("form#divide").submit(function(event){
+    event.preventDefault();
+    var divideNumber1 = parseInt($("#divide1").val());
+    var divideNumber2 = parseInt($("#divide2").val());
+    var divideResult = divide(divideNumber1, divideNumber2);
+    $("#divideOutput").text(divideResult);
   })
 
 });
